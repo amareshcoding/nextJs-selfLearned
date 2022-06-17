a8b39b3b93fabc509e3ac3961addad1ce59b84bf
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-06-17 23:49:29 +0530

    components and routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div >
-      <h1>wel</h1>
+      <div>
+        <div>
+          LOGO
+        </div>
+        <div>
+          <ul>
+            <li><Link href="/"><a>HOME</a></Link></li>
+            <li><Link href="/product"><a>PRODUCT</a></Link></li>
+            <li><Link href="/product/tech"><a>TECH</a></Link></li>
+            <li><Link href="/about"><a>ABOUT</a></Link></li>
+          </ul>
+        </div>
+      </div>
+      <h1>wellcom to next Js app</h1>
     </div>
   )
 }
---
+++ pages/product/tech.js
@@ -0,0 +1,7 @@
+export default function Tech(){
+     return(
+          <div>
+               <h1>welcome to tech page</h1>
+          </div>
+     )
+}
